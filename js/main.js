6afa8c9916e2fb4619f893bb3a2210d476f07fa7
Giovanna Ruiu <giovanna.ruiu@analogfolk.com>
2014-07-03 23:51:50 +0100

    Slider implemented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,68 @@
+       $(document).ready(function() {
+        $('.banner').show().revolution(
+        {
+            dottedOverlay:"none",
+            delay:3000,
+            startwidth:1170,
+            startheight:700,
+            hideThumbs:200,
+            
+            thumbWidth:100,
+            thumbHeight:50,
+            thumbAmount:5,
+            
+            navigationType:"bullet",
+            
+            touchenabled:"on",
+            onHoverStop:"on",
+            
+            swipe_velocity: 0.7,
+            swipe_min_touches: 1,
+            swipe_max_touches: 1,
+            drag_block_vertical: false,
+                                    
+            parallax:"mouse",
+            parallaxBgFreeze:"off",
+            parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
+                                    
+            keyboardNavigation:"off",
+            
+            navigationHAlign:"center",
+            navigationVAlign:"bottom",
+            navigationHOffset:0,
+            navigationVOffset:20,
+                    
+            shadow:0,
+            fullWidth:"on",
+            fullScreen:"on",
 
+            spinner:"spinner4",
+            
+            stopLoop:"off",
+            stopAfterLoops:-1,
+            stopAtSlide:-1,
+
+            shuffle:"off",
+            
+            autoHeight:"off",                       
+            forceFullWidth:"on",                       
+                                    
+                                                                        
+            hideThumbsOnMobile:"off",
+            hideNavDelayOnMobile:1500,                      
+            hideBulletsOnMobile:"off",
+            hideArrowsOnMobile:"off",
+            hideThumbsUnderResolution:0,
+            
+            hideSliderAtLimit:0,
+            hideCaptionAtLimit:0,
+            hideAllCaptionAtLilmit:0,
+            startWithSlide:0,
+            videoJsPath:"rs-plugin/videojs/",
+            fullScreenOffsetContainer: ""   
+        });
+     
+       });
 
 	// var $slides = $('#slides-container').find('section'),
 	// 	slideCount = $slides.length,
@@ -50,7 +114,3 @@
 	// 			// CHANGE TIMING FOR EACH SLIDE HERE
 	// 		}, 3000);
 	// 	}
-
-		
-
-});
